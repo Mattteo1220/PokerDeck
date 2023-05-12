98b1068a7d4135a2688e9a86b43dc460c698cd56
matt Mcentire <Msmcentire1220@outlook.com>
2023-05-12 11:25:38 -0600

    generate random players using fake name api

---
+++ PokerDeck.App/PokerDeck.Service.UnitTests/PlayerServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PokerDeck.Domain.UnitTests
+{
+    public class PlayerServiceTests
+    {
+        [Fact]
+        public void GeneratePlayers_Generates4Players()
+        {
+            var playerService = new PlayerService();
+            var expectedNumOfPlayers = 4;
+
+            var players = playerService.GeneratePlayers();
+
+            Assert.Equal(expectedNumOfPlayers, players.Count());
+            Assert.True(players.All(p => !string.IsNullOrWhiteSpace(p.Name)));
+        }
+    }
+}
---
+++ PokerDeck.App/PokerDeckService/PlayerService.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using PokerDeck.Domain.DTOS;
+using PokerDeck.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokerDeck.Domain
+{
+    public class PlayerService : IPlayerService
+    {
+        private const string _randomNameApiUrl = "https://api.namefake.com/english-unitedstates/random";
+        public PlayerService()
+        {
+
+        }
+
+        public IEnumerable<Player> GeneratePlayers()
+        {
+            var httpClient = new HttpClient();
+            var uri = new Uri(_randomNameApiUrl);
+            var players = new List<Player>();
+
+            try
+            {
+                for(var i = 0; i < 4; i++)
+                {
+                    var result = httpClient.Send(new HttpRequestMessage() { RequestUri = uri });
+                    var jsonResponse = result.Content.ReadAsStringAsync().Result;
+                    dynamic content = JsonConvert.DeserializeObject<dynamic>(jsonResponse);
+                    players.Add(new Player() { Name = content.name });
+                }
+            }
+            catch (Exception ex)
+            {
+                //Log message and throw 
+                throw;
+            }
+
+            return players;
+        }
+    }
+}
